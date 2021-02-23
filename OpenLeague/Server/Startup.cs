b50d2e2ce860e390fb29da9df837d88cd0de3e36
Bob Libby <bob.libby@lifetimebrands.com>
2021-02-22 19:03:17 -0500

    Setup swagger

--- OpenLeague/Server/Startup.cs
+++ OpenLeague/Server/Startup.cs
@@ -25,6 +25,7 @@ namespace OpenLeague.Server
 
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +42,12 @@ namespace OpenLeague.Server
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "OpenLeague API V1");
+            });
 
             app.UseHttpsRedirection();
             app.UseBlazorFrameworkFiles();
